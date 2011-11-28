8abc24212d5d7e96444c55e77717962828790526
Ken Earley <ken.earley@sentientbit.com>
2011-11-28 16:37:52 -0600

    Fixed rendering legend for bar chart. Added rendering labels separately.

--- lib/gchart/pie.rb
+++ lib/gchart/pie.rb
@@ -1,5 +1,13 @@
 module GChart
   class Pie < GChart::Base
+    
+    attr_accessor :labels
+
+    def initialize(*args, &block)
+      @labels = []
+      super
+    end
+
     # A single array of chart data. Raises +ArgumentError+
     # if more than one data set is provided.
     def data=(data)
@@ -10,12 +18,18 @@ module GChart
       super(data)
     end
     
+    # overrides GChart::Base#query_params
+    def query_params(params={}) #:nodoc:
+      render_labels(params)
+      super(params)
+    end
+  
     def render_chart_type #:nodoc:
       "p"
     end
     
-    def render_legend(params) #:nodoc:
-      params["chl"] = legend.join("|") if legend
+    def render_labels(params) #:nodoc:
+      params["chl"] = labels.join("|") if labels
     end
   end
 end
